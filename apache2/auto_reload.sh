f53443dcd812f22f72b1059dea5d170c1f3a4f22
Gabriele Pieretti <gabriele.pieretti@stiip.it>
2023-06-28 02:25:26 +0200

    improve apache

--- apache2/auto_reload.sh
+++ apache2/auto_reload.sh
@@ -4,7 +4,7 @@
 directory="/var/www/vhosts"
 
 # Controlla se ci sono file modificati negli ultimi 5 minuti
-if find "$directory" -type f -mmin -5 | read; then
+if find "$directory" -type f -mmin -2 | read; then
     # Esegui il comando "apache reload"
     /etc/init.d/apache2 reload
 fi
